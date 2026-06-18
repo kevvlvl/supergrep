20cb08d3b91cd0d3c26ec30c8624aaff017cae2d
kev <kev@notarealemail.com>
2026-06-17 23:18:55 -0400

    add tests

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ This was also my first experience using [zed](https://zed.dev/).
 
 ## How to run
 
+Run tests
+```shell
+cargo test
+```
+
+run the app
 ```shell
 cargo run "Del monte" searched-file.txt
 ```
--- src/config/config.rs
+++ src/config/config.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Config {
     pub pattern: String,
     pub file_to_parse: String,
@@ -16,5 +17,52 @@ impl Config {
             pattern,
             file_to_parse,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Config;
+
+    #[test]
+    fn test_build_success() {
+        let args = vec![
+            String::from("program_name"),
+            String::from("test_pattern"),
+            String::from("test_file.txt"),
+        ];
+        let config = Config::build(&args).unwrap();
+
+        assert_eq!(config.pattern, "test_pattern");
+        assert_eq!(config.file_to_parse, "test_file.txt");
+    }
+
+    #[test]
+    fn test_build_error_not_enough_args() {
+        let args = vec![String::from("program_name"), String::from("test_pattern")];
+        let config_result = Config::build(&args);
+
+        assert!(config_result.is_err());
+        assert_eq!(
+            config_result.unwrap_err(),
+            "Expected 2 arguments, Usage: supergrep <pattern> file_to_parse.txt"
+        );
+    }
+
+    #[test]
+    fn test_build_error_too_many_args() {
+        let args = vec![
+            String::from("program_name"),
+            String::from("test_pattern"),
+            String::from("test_file.txt"),
+            String::from("extra_arg"),
+        ];
+        let config_result = Config::build(&args);
+
+        assert!(config_result.is_err());
+        assert_eq!(
+            config_result.unwrap_err(),
+            "Expected 2 arguments, Usage: supergrep <pattern> file_to_parse.txt"
+        );
     }
 }
